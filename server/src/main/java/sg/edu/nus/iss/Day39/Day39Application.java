6f0a97f98dc9e8dcf1406492831fc9c91847825a
zh92 <zhehao92@gmail.com>
2023-05-05 09:21:28 +0800

    Initial commit

---
+++ server/src/main/java/sg/edu/nus/iss/Day39/Day39Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Day39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day39Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day39Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/sg/edu/nus/iss/Day39/Day39ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Day39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day39ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
